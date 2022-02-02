0fca70075b22a3fed9b66197c812d4eea2fe9f68
jpdreamthug <lentorilp@gmail.com>
2022-02-03 00:23:56 +0100

    init:start project

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path")
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      }
+    ]
+  }  
+}
+
