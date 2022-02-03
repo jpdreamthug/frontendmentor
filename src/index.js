a5bd7e4708efa367e059fba72e792e2729b89b13
jpdreamthug <lentorilp@gmail.com>
2022-02-03 01:13:19 +0100

    init: install MiniCssExtractPlugin

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-import './style.css';
-
+import './test.css';
+import './otherTest.css'
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: 'development',
@@ -17,9 +18,14 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       }
     ]
-  }  
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    }),
+  ],  
 }
 
